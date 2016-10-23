3049e519d9a8391c6bb1be3c02a97cfdbe37bde0
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-10-23 10:09:46 -1000

    Update iofunctions.h

--- projects/Project_1/iofunctions.h
+++ projects/Project_1/iofunctions.h
@@ -2,14 +2,6 @@
  *
  * NAME:            Gabriel Viloria
  *
- * HOMEWORK:        Project 1
- *
- * CLASS:           ICS 212
- *
- * INSTRUCTOR:      Ravi Narayan
- *
- * DATE:            March 4, 2016
- *
  * FILE:            iofunctions.h
  *
  * DESCRIPTION:     This header file contains the function prototypes for the iofunctions.c
